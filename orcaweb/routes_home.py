c1b83cd464c98455fd577206c594273244f40ac0
Michael Lawson <michael.lawson@linewize.com>
2017-02-09 10:00:13 +1300

    orcaweb

--- orcaweb/routes_home.py
+++ orcaweb/routes_home.py
@@ -4,6 +4,7 @@ import uuid
 import flask
 from flask import render_template
 from flask import request
+from flask import redirect
 
 from orcaweb import app
 from orcaweb.services import api__trainer
@@ -14,10 +15,145 @@ def route_dashboard():
     return render_template("dashboard.html", configuration=api__trainer.get__configuration__applications())
 
 
-@app.route("/application/<name>", methods=["GET"])
-def route_application(name):
-    return render_template("application.html", application=api__trainer.get__configuration__applications_app(name))
+@app.route("/servers")
+def route_servers():
+    return render_template("servers.html")
 
+
+@app.route("/applications")
+def route_applications_get():
+    return flask.jsonify(applications=api__trainer.get__configuration__applications())
+
+
+@app.route("/audit")
+def route_audit():
+    return render_template("audit.html", audit=api__trainer.get__audit())
+
+
+@app.route("/settings")
+def route_settings():
+    return render_template("settings.html", cloud=api__trainer.get__configuration__cloud())
+
+
+@app.route("/ajax/settings", methods=["GET"])
+def ajax__route_settings():
+    return flask.jsonify(api__trainer.get__configuration__cloud())
+
+@app.route("/ajax/servers", methods=["GET"])
+def ajax__route_servers():
+    servers = api__trainer.get__status__servers()
+    ret = []
+    for server in servers.values():
+        ret.append(server)
+    return flask.jsonify(servers=ret)
+
+
+@app.route("/ajax/settings", methods=["POST"])
+def ajax__route_settings_post():
+    api__trainer.set__configuration__cloud(request.json)
+    return ""
+
+@app.route("/ajax/application/<name>", methods=["GET"])
+def ajax__route_application(name):
+    return flask.jsonify(api__trainer.get__configuration__applications_app(name))
+
+@app.route("/ajax/application/<name>/configuration", methods=["GET"])
+def ajax__route_application_configuration(name):
+    return flask.jsonify(api__trainer.get__configuration__applications_app__config(name))
+
+
+@app.route("/ajax/application/<name>", methods=["POST"])
+def ajax__route_application_post(name):
+    api__trainer.set__configuration__applications_app(name, request.json)
+    return ""
+
+
+@app.route("/ajax/application/<name>/configuration", methods=["POST"])
+def ajax__route_application_post_configuration(name):
+    api__trainer.set__configuration__applications_app__config(name, request.json)
+    return ""
+
+
+@app.route("/settings", methods=["POST"])
+def route_settings_save():
+    cloud_provider_configuration = {
+        "Type": request.form.get("Type"),
+        "MinInstances": int(request.form.get("MinInstances")),
+        "MaxInstances": int(request.form.get("MaxInstances"))
+    }
+
+    if cloud_provider_configuration["Type"] == "AWS":
+        cloud_provider_configuration["AWSConfiguration"] = {
+            "Key": request.form.get("Key"),
+            "Secret": request.form.get("Secret"),
+            "Region": request.form.get("Region"),
+            "AMI": request.form.get("AMI"),
+            "SecurityGroupId": request.form.get("SecurityGroupId")
+        }
+
+    api__trainer.set__configuration__cloud(cloud_provider_configuration)
+    return redirect("/settings")
+
+
+@app.route("/application/<name>/scaling", methods=["GET"])
+def route_application_scaling(name):
+    return render_template("application_settings.html", name=name)
+
+@app.route("/application/<name>/configuration", methods=["GET"])
+def route_application_configuration(name):
+    return render_template("application_configuration.html", name=name)
+
+
+@app.route("/application/<name>/events", methods=["GET"])
+def route_application_events(name):
+    return render_template("application_events.html", name=name)
+
+
+@app.route("/application/<name>/server/<serverid>", methods=["GET"])
+def route_application_server(name, serverid):
+    return render_template("application_server.html", name=name, serverid=serverid)
+
+
+@app.route("/application/<name>/overview", methods=["GET"])
+def route_application_overview(name):
+    return render_template("application_overview.html", name=name)
+
+
+@app.route("/ajax/application/<name>/servers", methods=["GET"])
+def ajax__route_application_servers(name):
+    servers = []
+    cloud_layout = api__trainer.get__configuration__cloud_state()
+    for hostid, server in cloud_layout["Current"]["Layout"].items():
+        for name, application in server["Apps"].items():
+            if name == name:
+                servers.append(server)
+    return flask.jsonify(servers=servers)
+
+
+@app.route("/ajax/application/<name>/servers/desired", methods=["GET"])
+def ajax__route_application_servers_desired(name):
+    servers = []
+    cloud_layout = api__trainer.get__configuration__cloud_state()
+    for hostid, server in cloud_layout["Desired"]["Layout"].items():
+        for name, application in server["Apps"].items():
+            if name == name:
+                servers.append(server)
+    return flask.jsonify(servers=servers)
+
+
+@app.route("/ajax/application/<name>/servers/memory", methods=["GET"])
+def ajax__route_application_servers_memory(name):
+    return flask.jsonify(memory=api__trainer.get__status__application_statistics(name))
+
+
+@app.route("/ajax/application/<name>/count", methods=["GET"])
+def ajax__route_application_count(name):
+    return flask.jsonify(count=api__trainer.get__status__application_count(name))
+
+
+@app.route("/ajax/application/<name>/events", methods=["GET"])
+def ajax__route_application_events(name):
+    return flask.jsonify(events=api__trainer.get__status__application_events(name))
 
 def extract_command(string_command):
     p = re.compile('([a-z]+)\s(.*)')
@@ -27,10 +163,8 @@ def extract_command(string_command):
 
 @app.route("/application", methods=["POST"])
 def route_application__add():
-    app = {
-        "Name": request.form.get("name")
-    }
-    api__trainer.set__configuration__applications_app(app)
+    application = request.json
+    api__trainer.set__configuration__applications_app(application["Name"], application)
     return ""
 
 
@@ -46,6 +180,10 @@ def route_application__set(name):
     existing_configuration["DockerConfig"]["Repository"] = request.form.get("Repository")
     existing_configuration["DockerConfig"]["Reference"] = request.form.get("Reference")
 
+    existing_configuration["Needs"]["MemoryNeeds"] = int(request.form.get("MemoryNeeds"))
+    existing_configuration["Needs"]["CpuNeeds"] = int(request.form.get("CpuNeeds"))
+    existing_configuration["Needs"]["NetworkNeeds"] = int(request.form.get("NetworkNeeds"))
+
     existing_configuration["LoadBalancer"] = request.form.get("LoadBalancer")
     existing_configuration["Network"] = request.form.get("Network")
 
@@ -53,3 +191,19 @@ def route_application__set(name):
     return flask.redirect("/application/" + name)
 
 
+@app.route("/application/<name>/configuration", methods=["POST"])
+def route_application__set_configuration(name):
+    existing_configuration = api__trainer.get__configuration__applications_app__config(name)
+    existing_configuration["DockerConfig"]["Tag"] = request.form.get("Tag")
+    existing_configuration["DockerConfig"]["Repository"] = request.form.get("Repository")
+    existing_configuration["DockerConfig"]["Reference"] = request.form.get("Reference")
+
+    existing_configuration["Needs"]["MemoryNeeds"] = int(request.form.get("MemoryNeeds"))
+    existing_configuration["Needs"]["CpuNeeds"] = int(request.form.get("CpuNeeds"))
+    existing_configuration["Needs"]["NetworkNeeds"] = int(request.form.get("NetworkNeeds"))
+
+    existing_configuration["LoadBalancer"] = request.form.get("LoadBalancer")
+    existing_configuration["Network"] = request.form.get("Network")
+
+    api__trainer.set__configuration__applications_app__config(name, existing_configuration)
+    return flask.redirect("/application/" + name)
--- orcaweb/services/api__trainer.py
+++ orcaweb/services/api__trainer.py
@@ -2,15 +2,70 @@ import json
 
 import requests
 
+from orcaweb import app
+
 
 def get__configuration__applications():
-    response = requests.get("http://localhost:5000/state/config/applications")
+    response = requests.get("%s/config/applications" % app.config["ORCA_REWRITE_URL"])
+    return json.loads(response.text)
+
+
+def get__audit():
+    response = requests.get("%s/audit" % app.config["ORCA_REWRITE_URL"])
     return json.loads(response.text)
 
 
 def get__configuration__applications_app(name):
-    return get__configuration__applications()[name]
+    for app in get__configuration__applications():
+        if app["Name"] == name:
+            return app
+    return None
 
 
-def set__configuration__applications_app(object):
-    requests.post("http://localhost:5000/state/config/applications", data=json.dumps(object))
+def set__configuration__applications_app(name, object):
+    requests.post("%s/config/applications?application=%s" % (app.config["ORCA_REWRITE_URL"], name), data=json.dumps(object))
+
+
+def get__configuration__applications_app__config(name):
+    response = requests.get("%s/config/applications/configuration/latest?application=%s" % (app.config["ORCA_REWRITE_URL"], name))
+    return json.loads(response.text)
+
+
+def get__status__servers():
+    response = requests.get("%s/state" % (app.config["ORCA_REWRITE_URL"]))
+    return json.loads(response.text)
+
+
+def set__configuration__applications_app__config(appName, object):
+    requests.post("%s/config/applications/configuration/latest?application=%s" % (app.config["ORCA_REWRITE_URL"], appName), data=json.dumps(object))
+
+
+def get__configuration__cloud():
+    response = requests.get("%s/state/config/cloud" % app.config["ORCA_REWRITE_URL"])
+    return json.loads(response.text)
+
+
+def get__configuration__cloud_state():
+    response = requests.get("%s/state/cloud" % app.config["ORCA_REWRITE_URL"])
+    return json.loads(response.text)
+
+
+def set__configuration__cloud(object):
+    requests.post("%s/state/config/cloud" % app.config["ORCA_REWRITE_URL"], data=json.dumps(object))
+
+
+def get__status__application_statistics(application_name):
+    response = requests.get(
+        "%s/state/cloud/application/performance?application=%s" % (app.config["ORCA_REWRITE_URL"], application_name))
+    return json.loads(response.text)
+
+
+def get__status__application_count(application_name):
+    response = requests.get(
+        "%s/state/cloud/application/count?application=%s" % (app.config["ORCA_REWRITE_URL"], application_name))
+    return json.loads(response.text)
+
+
+def get__status__application_events(application_name):
+    response = requests.get("%s/audit?application=%s" % (app.config["ORCA_REWRITE_URL"], application_name))
+    return json.loads(response.text)
